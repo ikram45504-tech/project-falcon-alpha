1b592addbd8500ba0ef80f907f2e5f595ec4f2e5
ikram45504-tech <ikram45504@gmail.com>
2026-08-19 00:13:34 +0500

    Restore normal dashboard navigation entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,4 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import TemporaryWorkspaceNavigation from "./TemporaryWorkspaceNavigation";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <>
-    <TemporaryWorkspaceNavigation />
-    <App />
-  </>
-);
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(<App />);
